e768e56ba04116e1382bca145100983d7130ddc4
Juan Lulkin <maiz@lulk.in>
2016-11-09 14:39:51 +0200

    Fix ramda path on build

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-import curryN from './curryN'
+import curryN from 'ramda/src/curryN'
 
 export default curryN(4, (builder, handler, eventSource, next) =>
   eventSource((value) => builder(handler, next, value))
