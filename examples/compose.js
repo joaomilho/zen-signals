b15ddb605390867a2a70dbcbdbb5f8032c79c2b6
Juan Lulkin <maiz@lulk.in>
2020-06-26 17:07:50 +0200

    Move code to /src

--- examples/compose.js
+++ examples/compose.js
@@ -1,16 +1,13 @@
-import fold from '../fold'
-import map from '../map'
-import signal from '../signal'
-import add from 'ramda/src/add'
-import pipe from 'ramda/src/pipe'
+import fold from "../fold";
+import map from "../map";
+import signal from "../signal";
+import add from "ramda/src/add";
+import pipe from "ramda/src/pipe";
 
-const send = pipe(
-  map(add(1)),
-  fold(add, 0)
-)(signal())(console.log)
+const send = pipe(map(add(1)), fold(add, 0))(empty())(console.log);
 
-send(1) // 2
-send(2) // 5
-send(3) // 9
-send(4) // 14
-send(5) // 20
+send(1); // 2
+send(2); // 5
+send(3); // 9
+send(4); // 14
+send(5); // 20
